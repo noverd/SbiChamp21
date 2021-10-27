95d58ef843033130eb41287919f5721a74501bbd
Noverd <dasfasru@gmail.com>
2021-10-27 19:57:36 +0300

    Каамм

--- my_strategy2.py
+++ my_strategy2.py
@@ -13,12 +13,13 @@ class MyStrategy:
     Смотри подсказки по ее улучшению, оформленные в виде специальных комментариев: # TODO ...
     '''
     black_list_planet = []
-
+    foundry_list_planet = []
+    furnace_list_planet  = []
+    bioreactor_list_planet = []
     def get_action(self, game: Game) -> Action:
-
-        global planet_index
         moves = []
         builds = []
+        global planet_index, planet_index_order
         resource_planets = []
         stone_planets = []
         ore_planets = []
@@ -37,44 +38,82 @@ class MyStrategy:
             elif i.harvestable_resource == Resource.SAND:
                 resource_planets.append(x)
                 sand_planets.append(x)
-        mill = 1110000000000000000000
-        for x, i in enumerate(resource_planets):
-            if not i in self.black_list_planet:
-                rast = dist((game.planets[i].x, game.planets[i].y,), (0, 0))
-                print(f"Дистанция {rast}")
-                if rast < mill:
-                    mill = rast
-                    planet_index = i
-                    list_del_index = x
-                    print(f"АХахаххаха: {list_del_index}")
-                    planet = game.planets[i]
-                    if game.planets[0].resources.STONE > 49:
-                        self.black_list_planet.append(planet_index)
-                        moves.append(MoveAction(0, planet_index, 50, Resource.STONE))
-                    if planet.harvestable_resource == Resource.STONE:  # Строим каменоломлю если есть ресурс STONE
-                        builds.append(BuildingAction(planet_index, BuildingType.QUARRY))
-                    elif planet.harvestable_resource == Resource.ORE:  # Строим шахту если есть ресурс ORE
-                        builds.append(BuildingAction(planet_index, BuildingType.MINES))
-                    elif planet.harvestable_resource == Resource.SAND:  # Строим карьер если есть ресурс SAND
-                        builds.append(BuildingAction(planet_index, BuildingType.CAREER))
-                    elif planet.harvestable_resource == Resource.ORGANICS:
-                        builds.append(BuildingAction(planet_index, BuildingType.FARM))
-        print(self.black_list_planet)
-        print(builds)
+        try:
+            if game.planets[0].resources[Resource.STONE] > 149:
+                print("Провервка")
+        except KeyError:
+            print("Хуч")
+        else:
+            print(game.planets[0].resources)
+            if game.planets[0].resources[Resource.STONE] > 149:
 
-        print(f"Планеты с ресурсами: {resource_planets}")
-        print(f"Планеты с камнем: {stone_planets}")
-        print(f"Планеты с песком: {sand_planets}")
-        print(f"Планеты с органикой: {organic_planets}")
-        print(f"Планеты с рудой: {ore_planets}")
-        for planet_index, planet in enumerate(game.planets):
-            if planet.harvestable_resource == Resource.STONE:  # Строим каменоломлю если есть ресурс STONE
-                builds.append(BuildingAction(planet_index, BuildingType.QUARRY))
-            elif planet.harvestable_resource == Resource.ORE:  # Строим шахту если есть ресурс ORE
-                builds.append(BuildingAction(planet_index, BuildingType.MINES))
-            elif planet.harvestable_resource == Resource.SAND:  # Строим карьер если есть ресурс SAND
-                builds.append(BuildingAction(planet_index, BuildingType.CAREER))
-            elif planet.harvestable_resource == Resource.ORGANICS:
-                builds.append(BuildingAction(planet_index, BuildingType.FARM))
+                mill = 1110000000000000000000
+                for x, i in enumerate(resource_planets):
+                    if not i in self.black_list_planet:
+                        rast = dist((game.planets[i].x, game.planets[i].y,), (0, 0))
+                        print(f"Дистанция {rast}")
+                        print(f"ПРошлая дистация: {mill}")
+                        if rast < mill:
+                            mill = rast
+                            planet_index = i
+                            list_del_index = x
+                            print(f"АХахаххаха: {list_del_index}")
+                            planet = game.planets[i]
+                moves.append(MoveAction(0, planet_index, 50, Resource.STONE))
+                self.black_list_planet.append(planet_index)
 
-        return Action(moves, builds)
+
+
+
+                mill = 1110000000000000000000
+                for x, i in enumerate(game.planets):
+                    if not i.id in self.black_list_planet :
+                        i = i.id
+                        print(i)
+                        rast = dist((game.planets[i].x, game.planets[i].y,), (game.planets[planet_index].x, game.planets[planet_index].y))
+                        print(f"Дистанция {rast}")
+                        print(f"ПРошлая дистация: {mill}")
+                        if rast < mill:
+                            mill = rast
+                            planet_index_order = i
+                            planet = game.planets[i]
+
+                            print(planet_index_order)
+                moves.append(MoveAction(0, planet_index_order, 100, Resource.STONE))
+                print(f"\ОРден: {planet_index_order}")
+
+                self.black_list_planet.append(planet_index)
+                print(self.black_list_planet)
+                print(builds)
+                match game.planets[planet_index].harvestable_resource:
+                    case Resource.ORE:
+                        self.foundry_list_planet.append(planet_index_order)
+                    case Resource.SAND:
+                        self.furnace_list_planet.append(planet_index_order)
+                    case Resource.ORGANICS:
+                        self.bioreactor_list_planet.append(planet_index_order)
+
+
+        for i in self.black_list_planet:
+
+            planet = game.planets[i]
+            match planet.harvestable_resource:
+                case Resource.STONE:
+                    builds.append(BuildingAction(i, BuildingType.QUARRY))
+                case Resource.ORE:
+                    builds.append(BuildingAction(i, BuildingType.MINES))
+                case Resource.SAND:
+                    builds.append(BuildingAction(i, BuildingType.CAREER))
+                case Resource.ORGANICS:
+                    builds.append(BuildingAction(i, BuildingType.FARM))
+        for i in self.foundry_list_planet:
+            builds.append(BuildingAction(i, BuildingType.FOUNDRY))
+        for i in self.furnace_list_planet:
+            builds.append(BuildingAction(i, BuildingType.FURNACE))
+        for i in self.bioreactor_list_planet:
+            builds.append(BuildingAction(i, BuildingType.BIOREACTOR))
+        print(self.bioreactor_list_planet)
+        print(self.furnace_list_planet)
+        print(self.foundry_list_planet)
+
+        return Action(moves, builds, None)
